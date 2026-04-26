8e63acb55bdded43dc91ec92ea8ecb9a3e952ff0
Beichen-H <beichen7@illinois.edu>
2026-04-26 03:45:18 +0000

    Deploying to gh-pages from @ Beichen-H/Beichen-H.github.io@f21343a25ed040c7d260592e833306c4265f6bf2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,15 +405,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-rag-academic-advising-agent",
+            },},{id: "projects-rag-academic-advising-agent",
           title: 'RAG Academic Advising Agent',
           description: "An AI agent designed to automate complex academic advising inquiries at UIUC.",
           section: "Projects",handler: () => {
